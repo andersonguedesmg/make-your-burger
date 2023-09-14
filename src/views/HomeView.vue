4387d75b2868a3278c37dbb5740aa2ef47bba0cc
Anderson Guedes <anderson.guedes@outlook.com.br>
2023-09-14 19:15:53 -0300

    feat(Make Your Burger): Estruturação do Form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <Banner />
   <div class="main-container">
-    <h1>Home!</h1>
+    <h1>Monte seu Burger!</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurgerForm.vue";
 
 export default {
   name: "HomeView",
-  components: { Banner },
+  components: { Banner, BurgerForm },
 };
 </script>
